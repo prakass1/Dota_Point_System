2c23d44f407e8b4b2116b56a71f3efbbbd441040
Kaveen Kumar <2idli1vada@email.com>
2020-11-12 19:26:59 +0100

    added comments

--- points_calculation.py
+++ points_calculation.py
@@ -1,15 +1,19 @@
 import buff_extraction
 
-#points distribution
+#points distribution per player per match
+#calculate kill points
 def calc_killp(kills):
     return kills * 10
 
+#calculate assist points
 def calc_assistp(assist):
     return assist * 5
 
+#calculate death points
 def calc_deathsp(deaths):
     return deaths * -2
 
+#calculate last hit points
 def calc_lhp(lh):
     if lh < 50:
         lhp = 2
@@ -25,6 +29,7 @@ def calc_lhp(lh):
         lhp = 10
     return lhp
 
+#calculate gold per minute points
 def calc_gpmp(gpm):
     if gpm < 500:
         gpmp = 2
@@ -40,6 +45,7 @@ def calc_gpmp(gpm):
         gpmp = 10
     return gpmp
 
+#calculate building damage points
 def calc_bldp(bld):
     if bld < 2000:
         bldp = 2
@@ -55,6 +61,7 @@ def calc_bldp(bld):
         bldp = 10
     return bldp
 
+#calculate heal points
 def calc_healp(heal):
     if heal < 2000:
         healp = 2
@@ -72,20 +79,35 @@ def calc_healp(heal):
         healp = 10
     return healp
 
+
 for match, player in buff_extraction.extraction_data['matches'].items():
     for stat in player:
+        
+        #kill
         k = int(stat['kills'])
         kp = calc_killp(k)
+
+        #death
         d = int(stat['deaths'])
         dp = calc_deathsp(d)
+
+        #assist
         a = int(stat['assist'])
         ap = calc_assistp(a)
+
+        #last hit
         lh = int(stat['lh'])
         lhp = calc_lhp(lh)
+
+        #gold per minute
         gpm = int(stat['gpm'])
         gpmp = calc_gpmp(gpm)
+
+        #building damage (values are with string 'k', so hast to be updated)
         #bld = int(float(stat['bld']))
         #bldp = calc_bldp(bld)
+
+        #heal points (some values in heal are '-')
         if stat['heal'] == "-":
             healp = 0
         else:
